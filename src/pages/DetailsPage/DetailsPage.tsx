d15bea33f43f5c737c59bb1e1505906e0bdaad54
Alesia <alesijbusch@gmail.com>
2023-05-04 00:48:12 +0300

    refactor(DetailsPage): add media queries to DetailsPage

--- src/pages/DetailsPage/DetailsPage.tsx
+++ src/pages/DetailsPage/DetailsPage.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { Content } from 'ui';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Content } from "ui";
 import {
+  Details,
   DetailsGenre,
   DetailsPoster,
   DetailsPosterGroup,
@@ -13,10 +14,15 @@ import {
   StyledFavorites,
   StyledFavoritesIcon,
   StyledImdbIcon,
-} from './styles';
-import { faBookmark, faCircle } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { spawn } from 'child_process';
+  Table,
+  Tbody,
+  TdCellName,
+  TdCellValue,
+  Tr,
+} from "./styles";
+import { faBookmark, faCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { spawn } from "child_process";
 
 interface MovieProps {
   Title: string;
@@ -40,7 +46,7 @@ export const DetailsPage = () => {
   const [movie, setMovie] = useState<MovieProps | undefined>();
   const { name } = useParams();
 
-  const movieKey = '73417f5e';
+  const movieKey = "73417f5e";
 
   useEffect(() => {
     fetch(`https://www.omdbapi.com/?apikey=${movieKey}&i=${name}&plot=full`)
@@ -51,25 +57,63 @@ export const DetailsPage = () => {
 
   return (
     <Content>
-      <DetailsGenre>
-        {movie?.Genre.split(',').join(` * `)}
-        <FontAwesomeIcon icon={faCircle} />
-      </DetailsGenre>
-      <DetailsTitle>{movie?.Title}</DetailsTitle>
-      <StickersGroup>
-        <StickerReating>{movie?.imdbRating}</StickerReating>
-        <StickerDefault>
-          <StyledImdbIcon /> {movie?.imdbRating}
-        </StickerDefault>
-        <StickerDefault>{movie?.Runtime}</StickerDefault>
-      </StickersGroup>
-      <DetailsPosterGroup>
-        <DetailsPoster src={movie?.Poster} alt="" />
-        <StyledFavorites>
-          <StyledFavoritesIcon icon={faBookmark} />
-        </StyledFavorites>
-      </DetailsPosterGroup>
-      <Plot>{movie?.Plot}</Plot>
+      <Details>
+        <DetailsGenre>
+          {movie?.Genre.split(",").join(`, `)}
+          {/* <FontAwesomeIcon icon={faCircle} /> */}
+        </DetailsGenre>
+        <DetailsTitle>{movie?.Title}</DetailsTitle>
+        <StickersGroup>
+          <StickerReating>{movie?.imdbRating}</StickerReating>
+          <StickerDefault>
+            <StyledImdbIcon /> {movie?.imdbRating}
+          </StickerDefault>
+          <StickerDefault>{movie?.Runtime}</StickerDefault>
+        </StickersGroup>
+        <DetailsPosterGroup>
+          <DetailsPoster src={movie?.Poster} alt="" />
+          <StyledFavorites>
+            <StyledFavoritesIcon icon={faBookmark} />
+          </StyledFavorites>
+        </DetailsPosterGroup>
+        <Plot>{movie?.Plot}</Plot>
+        <Table>
+          <Tbody>
+            <Tr>
+              <TdCellName>Year</TdCellName>
+              <TdCellValue>{movie?.Year}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Released</TdCellName>
+              <TdCellValue>{movie?.Released}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>BoxOffice</TdCellName>
+              <TdCellValue>{movie?.BoxOffice}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Country</TdCellName>
+              <TdCellValue>{movie?.Country}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Production</TdCellName>
+              <TdCellValue>{movie?.Production}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Actors</TdCellName>
+              <TdCellValue>{movie?.Actors}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Director</TdCellName>
+              <TdCellValue>{movie?.Director}</TdCellValue>
+            </Tr>
+            <Tr>
+              <TdCellName>Writers</TdCellName>
+              <TdCellValue>{movie?.Writer}</TdCellValue>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Details>
     </Content>
   );
 };
--- src/pages/DetailsPage/styles.ts
+++ src/pages/DetailsPage/styles.ts
@@ -1,21 +1,41 @@
-import styled from 'styled-components';
-import { ImdbIcon } from 'assets';
+import styled from "styled-components";
+import { ImdbIcon } from "assets";
 
-import { spaces, tokens, typography } from 'ui';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { spaces, tokens, typography } from "ui";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const { color, layout } = tokens;
 
+const Details = styled.div`
+  ${layout.media.tablet} {
+    display: grid;
+    grid-template-areas:
+      "my-poster my-genre"
+      "my-poster my-title"
+      "my-poster my-sticker"
+      "my-poster my-plot"
+      "my-poster my-table";
+  }
+`;
 const DetailsGenre = styled.p`
-  ${typography.B1}
+  ${typography.B1};
   ${color.text.light};
+  ${layout.media.tablet} {
+    grid-area: my-genre;
+  }
 `;
 const DetailsTitle = styled.h1`
-  ${typography.H1}
-  ${spaces.mb24}
+  ${typography.H1};
+  ${spaces.mb24};
+  ${layout.media.tablet} {
+    grid-area: my-title;
+  }
 `;
 const StickersGroup = styled.div`
   display: flex;
   ${spaces.mb40}
+  ${layout.media.tablet} {
+    grid-area: my-sticker;
+  }
 `;
 const StickerReating = styled.div`
   border-radius: 6px;
@@ -35,11 +55,29 @@ const StyledImdbIcon = styled(ImdbIcon)`
   margin-right: 8px;
 `;
 const DetailsPosterGroup = styled.div`
-  ${spaces.mb40}
+  ${spaces.mb40};
+
+  ${layout.media.tablet} {
+    width: 208px;
+    margin-right: 32px;
+    grid-area: my-poster;
+  }
+  ${layout.media.desktop} {
+    width: 266px;
+    margin-right: 42px;
+  }
 `;
 const DetailsPoster = styled.img`
   width: 100%;
+  border-radius: 20px;
+  display: block;
   ${spaces.mb24}
+  ${layout.media.smallTablet} {
+    width: 60%;
+  }
+  ${layout.media.tablet} {
+    width: 100%;
+  }
 `;
 const StyledFavoritesIcon = styled(FontAwesomeIcon)`
   font-size: 18px;
@@ -56,9 +94,46 @@ const StyledFavorites = styled.div`
       ${color.text.white};
     }
   }
+  ${layout.media.tablet} {
+    padding: 19px 45px;
+  }
 `;
 const Plot = styled.p`
   ${typography.B1};
+  ${spaces.mb40};
+  grid-area: my-plot;
+  ${layout.media.largeDesktop} {
+    margin-right: 310px;
+  }
+`;
+const Table = styled.table`
+  ${spaces.mb56};
+
+  ${layout.media.tablet} {
+    grid-area: my-table;
+  }
+`;
+const Tbody = styled.tbody``;
+const Tr = styled.tr`
+  display: grid;
+  grid-template-columns: 148px auto;
+  margin-bottom: 16px;
+  :last-child {
+    margin-bottom: 0;
+  }
+  ${layout.media.tablet} {
+    margin-bottom: 20px;
+    grid-template-columns: 136px auto;
+  }
+`;
+const TdCellName = styled.td`
+  ${typography.S3};
+  ${color.text.light};
+`;
+const TdCellValue = styled.td`
+  flex-basis: auto;
+  ${typography.B1};
+  ${color.text.white};
 `;
 
 export {
@@ -73,4 +148,10 @@ export {
   StyledFavorites,
   DetailsPosterGroup,
   Plot,
+  Table,
+  Tbody,
+  Tr,
+  TdCellName,
+  TdCellValue,
+  Details,
 };
